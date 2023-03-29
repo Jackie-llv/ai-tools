26a116908c35cc4a4acedc3fc2b73a4e99a4330b
lvjiaqi <lvjiaqi@itcast.cn>
2023-03-29 17:57:11 +0800

    fix: bugfix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 const Layout = (props: { children?: JSX.Element | JSX.Element[] }) => {
   return (
