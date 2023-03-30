c9a221f200e6549f2c9093bdaf7ef7d16131ad2c
lvjiaqi <lvjiaqi@itcast.cn>
2023-03-30 21:43:03 +0800

    feat: 模拟面试

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -8,9 +8,9 @@ export const config = {
 const handler = async (req: NextRequest): Promise<Response> => {
   const params = await req.json()  
 
-  if (!params.userInput) {
-    return new Response('Invalid user input', { status: 400 })
-  }
+  // if (!params.userInput) {
+  //   return new Response('Invalid user input', { status: 400 })
+  // }
 
   const payload: OpenAIStreamPayload = {
     model: 'gpt-3.5-turbo',
