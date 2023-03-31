87ecebbe6ac2f7837565711024c624fb214d07cb
lvjiaqi <lvjiaqi@itcast.cn>
2023-03-31 09:56:10 +0800

    fix: bugfix

--- pages/chat/[id].tsx
+++ pages/chat/[id].tsx
@@ -65,12 +65,12 @@ export default function Chat(params: { id: string}) {
     
     setLoading(true)
     generate([
-      { role: 'system', content: `你现在是一个前端面试官，接下来我们来模拟面试，现在请你问我前端面试题中${params.id}相关的面试题，如果你认为我回答正确，那么就给一个正面的回答并且继续问下一道题，如果回答错了，那么请纠正，并告诉我正确答案，然后再继续问下一道。如果我表示我不知道答案，那么你就直接告诉我，然后再问下一道题，接下来就直接开始，请你出第一道题（注意：接下来的过程中问的所有的题都只是html阶段的题，并且你需要不停的问我问题，直到我说结束，不用回答好的，直接开始）`},
+      { role: 'system', content: `你现在是一个前端面试官，接下来我们来模拟面试，现在请你问我前端面试题中${params.id}相关的面试题，如果你认为我回答正确，那么你就给我一个正面的反馈，如果回答错了，那么请纠正，并告诉我正确答案。如果我表示我不知道答案，那么你就直接告诉我答案。接下来就直接开始，请注意你需要每次都出不一样的题目来问我`},
       { role: 'user', content: `接下来请开始${params.id}阶段的面试`}
     ]).then(() => {
       setTimeout(() => {
         setMsgList([
-          { role: 'system', content: `你现在是一个前端面试官，接下来我们来模拟面试，现在请你问我前端面试题中${params.id}相关的面试题，如果你认为我回答正确，那么就给一个正面的回答并且继续问下一道题，如果回答错了，那么请纠正，并告诉我正确答案，然后再继续问下一道。如果我表示我不知道答案，那么你就直接告诉我，然后再问下一道题，接下来就直接开始，请你出第一道题（注意：接下来的过程中问的所有的题都只是html阶段的题，并且你需要不停的问我问题，直到我说结束，不用回答好的，直接开始）`},
+          { role: 'system', content: `你现在是一个前端面试官，接下来我们来模拟面试，现在请你问我前端面试题中${params.id}相关的面试题，如果你认为我回答正确，那么你就给我一个正面的反馈，如果回答错了，那么请纠正，并告诉我正确答案。如果我表示我不知道答案，那么你就直接告诉我答案。接下来就直接开始，请注意你需要每次都出不一样的题目来问我`},
           { role: 'user', content: `接下来请开始${params.id}阶段的面试`},
           { role: 'assistant', content: generatedResults[generatedResults.length - 1] }
         ])
@@ -84,15 +84,19 @@ export default function Chat(params: { id: string}) {
       return;
     }
     const msgs = [...sendMsg]
-    const list = [...msgList]
+    const list = [...msgList] // 1-s 2-u 3-a 4-u 5-a 6-s 7-a
     setLoading(true)
     msgs.push(inputValue)
     list.push({ role: 'user', content: inputValue })
     setSendMsg(msgs)
     setInputValue('')
-    // 请求
+    // 反馈
     await generate(list)
     list.push({ role: 'assistant', content: generatedResults[generatedResults.length - 1] })
+    list.push({ role: 'system', content: '请继续出题' })
+    // 继续
+    await generate(list)
+    setMsgList([...list])
     setLoading(false)
   }
 
