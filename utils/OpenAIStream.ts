e61e6f762365429b6d87096c0ee746dea3e7b0d1
lvjiaqi <715830277@qq.com>
2023-04-04 18:01:34 +0800

    add: 次数限制

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -32,8 +32,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
   const res = await fetch('https://api.openai.com/v1/chat/completions', {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ''}`,
-      'OpenAI-Organization': process.env.OPENAI_ORGANIZATION
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ''}`
     },
     method: 'POST',
     body: JSON.stringify(payload),
